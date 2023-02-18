6f97845cbaa47ccfa6c720db7238eddb98faee1d
Cristiana <cristiana.gherasim@gmail.com>
2023-02-18 18:19:51 -0500

    added API

--- eventsapi/src/services/EventsServices.ts
+++ eventsapi/src/services/EventsServices.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
-import { GetEvents } from '../model/Events';
+import { GetEventsData } from '../model/Events';
 
-export function GetEventDetails() : Promise<GetEvents> {
-    return axios.get<GetEvents>("https://app.ticketmaster.com/discovery/v2/events.json?apikey=MlsyU0dIeOhOWoaOEVqX3xT9QKxuvX40")
-    .then(response => response.data);
+export function GetEvents(reqData:{startDate:string,endDate:string,keyword:string,postalCode:string}) : Promise<Event[]> {
+    console.log(reqData);
+
+    const startDate = reqData.startDate === ""? "": `${reqData.startDate}T00:00:00Z`;
+    const endDate = reqData.endDate === ""? "": `${reqData.endDate}T23:59:59Z`;
+    return axios.get<Event[]>(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=MlsyU0dIeOhOWoaOEVqX3xT9QKxuvX40&keyword=${reqData.keyword}&postalCode=${reqData.postalCode}&startDateTime=${startDate}&endDateTime=${endDate}&size=5`)
+    .then((response:any) => response.data._embedded.events);
 }
