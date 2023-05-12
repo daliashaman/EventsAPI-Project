07104a353e70a6351a9f4acc37e3f2843aeb1bac
“Dalia <daliashaman@gmail.com>
2023-05-12 14:10:02 -0400

    updates

--- eventsapi/src/components/EventDetails.tsx
+++ eventsapi/src/components/EventDetails.tsx
@@ -1,62 +1,92 @@
-import { useEffect, useState } from 'react';
-import { Card, CardBody, CardTitle, CardSubtitle, CardText, Row, Col, Button } from 'reactstrap';
-import { useParams } from 'react-router-dom';
-import {GetEventById} from '../services/EventsServices';
-import {AnEvent} from "../model/Events";
+import { useEffect, useState } from "react";
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  Row,
+  Col,
+  Button,
+} from "reactstrap";
+import { useParams } from "react-router-dom";
+import { GetEventById } from "../services/EventsServices";
+import { AnEvent } from "../model/Events";
 
-export function EventDetails () {
-  const {id} = useParams<{id:string}>();
+export function EventDetails() {
+  const { id } = useParams<{ id: string }>();
 
   const [detailsRoute, setDetailsRoute] = useState<AnEvent | null>(null);
 
   useEffect(() => {
     async function fetchEvent() {
       const response = await GetEventById(id ?? "");
-      setDetailsRoute(response.data)
-    
+      setDetailsRoute(response.data);
     }
-    fetchEvent()
+    fetchEvent();
   }, [id]);
-
- 
 
   return (
     <div className="DetailsRoute">
-      {detailsRoute !==null ? (
+      {detailsRoute !== null ? (
         <Row>
           <Col>
-                <Card>
-                {detailsRoute.images[0] && (
-                  <img
-                    src={detailsRoute.images[0].url}
-                    alt="Event"
-                    style={{ height: "300px", width: "50vw" }}
-                  />
-                )}
-                <CardBody>
-                  <CardTitle tag="h5">{detailsRoute.name}</CardTitle>
-                  <CardSubtitle className="mb-2 text-muted" tag="h6">
-                    {detailsRoute.dates.start.localDate}
-                  </CardSubtitle>
-                  <CardText><b>Venue: </b>{detailsRoute?._embedded?.venues[0]?.name}</CardText>
-                  </CardBody>
-                  <div className="details">
-                    <p><b>Genre: </b>{detailsRoute?.classifications[0]?.genre.name}</p>
-                    <p><b>Address: </b>{detailsRoute?._embedded?.venues[0]?.address?.line1} </p>
-                    <p><b>City: </b>{detailsRoute?._embedded?.venues[0]?.city.name}</p>
-                    <p><b>State: </b>{detailsRoute?._embedded?.venues[0]?.state.name}</p>
-                    <p><b>Postal Code: </b>{detailsRoute?._embedded?.venues[0]?.postalCode}</p>
-                  </div>
-                  <Button href={detailsRoute.url} style={{width: "50vw" , backgroundColor: "green"}} target="_blank">
-                    Buy Tickets
-                  </Button>
-              </Card>
-              </Col>
-              </Row>
-      ):(
+            <Card>
+              {detailsRoute.images[0] && (
+                <img
+                  src={detailsRoute.images[0].url}
+                  alt="Event"
+                  style={{
+                    height: "300px",
+                    width: "40vw",
+                  }}
+                />
+              )}
+              <CardBody>
+                <CardTitle tag="h5">{detailsRoute.name}</CardTitle>
+                <CardSubtitle className="mb-2 text-muted" tag="h6">
+                  {detailsRoute.dates.start.localDate}
+                </CardSubtitle>
+                <CardText>
+                  <b>Venue: </b>
+                  {detailsRoute?._embedded?.venues[0]?.name}
+                </CardText>
+              </CardBody>
+              <div className="details">
+                <p>
+                  <b>Genre: </b>
+                  {detailsRoute?.classifications[0]?.genre.name}
+                </p>
+                <p>
+                  <b>Address: </b>
+                  {detailsRoute?._embedded?.venues[0]?.address?.line1}{" "}
+                </p>
+                <p>
+                  <b>City: </b>
+                  {detailsRoute?._embedded?.venues[0]?.city.name}
+                </p>
+                <p>
+                  <b>State: </b>
+                  {detailsRoute?._embedded?.venues[0]?.state.name}
+                </p>
+                <p>
+                  <b>Postal Code: </b>
+                  {detailsRoute?._embedded?.venues[0]?.postalCode}
+                </p>
+              </div>
+              <Button
+                href={detailsRoute.url}
+                style={{ width: "40vw", backgroundColor: "green" }}
+                target="_blank"
+              >
+                Buy Tickets
+              </Button>
+            </Card>
+          </Col>
+        </Row>
+      ) : (
         <h1>Loading...</h1>
-      )
-      }
+      )}
     </div>
   );
 }
